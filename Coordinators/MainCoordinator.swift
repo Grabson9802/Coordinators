b63a0e052c67898659b33171e9a4e6e3a841b351
Krystian Grabowy <krys9802@gmail.com>
2022-04-12 20:53:12 +0200

    MVVM-C testing

---
+++ Coordinators/MainCoordinator.swift
@@ -0,0 +1,36 @@
+//
+//  MainCoordinator.swift
+//  Coordinators
+//
+//  Created by Krystian Grabowy on 12/04/2022.
+//
+
+import Foundation
+import UIKit
+
+class MainCoordinator: Coordinator {
+    var childCoordinators = [Coordinator]()
+    var navigationController: UINavigationController
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        let vc = ViewController.instantiate()
+        vc.coordinator = self
+        navigationController.pushViewController(vc, animated: false)
+    }
+    
+    func buySubscription() {
+        let vc = BuyViewController.instantiate()
+        vc.coordinator = self
+        navigationController.pushViewController(vc, animated: true)
+    }
+    
+    func createAccount() {
+        let vc = CreateAccountViewController.instantiate()
+        vc.coordinator = self
+        navigationController.pushViewController(vc, animated: true)
+    }
+}
